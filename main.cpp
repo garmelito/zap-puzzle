9d937deff1b0dabd6d81f99b097b13da70a9faea
Piotr Wachowicz <pwachowicz98@gmail.com>
2018-03-05 12:46:57 +0100

    Added filling closedset, and add ifs to use it. Also filling path list

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@ using namespace std;
 //Potem dodam sprawdzanie czy inputBoard.txt dziala czy moze go nie ma
 //Zadanie moze nie miec rozwiazania. While powinien działac dopuki openset != NULL, a wewnatrz bedzie if porownujacy tablice z rozwiazaniem
 //Będę musiał jakoś podzielić program na moduły
-//Muszę usunąć listy na koniec
-//Muszę dodać warunek dodawania z closedsetem
 //Na koniec musi znajdować droge
 
 struct bond
@@ -22,9 +20,12 @@ int h (int board[][3]);
 int module (int number);
 int id (int board[][3]);
 void newBoard (int board[][3], int copy[][3], int luka_y, int luka_x, int obok_y, int obok_x);
-void newBond (bond *openset, int fRating, int board[][3]);
+void newBond (bond *openset, int fRating, int board[][3], int idRating);
 bool notHere (bond *fresh, bond *topic);
 void transferToClosedset (bond *&closedset, bond *openset);
+bool alreadyInside (bond *head, int id);
+bond *reconstructPath (bond *openset);
+void extermination (bond *&head);
 
 int main()
 {
@@ -53,7 +54,7 @@ int main()
     int copy_down[3][3];
     int copy_left[3][3];
     int copy_right[3][3];
-    int fRating;
+    int fRating, idRating;
     while (openset->id != goal)
     {
         for (int i=0; i<3; i++)
@@ -68,26 +69,42 @@ int main()
         if (luka_y != 0)
         {
             newBoard(openset->board, copy_up, luka_y, luka_x, luka_y-1, luka_x);
-            fRating = openset->g + 1 + h(copy_up);
-            newBond (openset, fRating, copy_up);
+            idRating = id(copy_up);
+            if (!(alreadyInside(openset, idRating) || alreadyInside(closedset, idRating)))
+            {
+                    fRating = openset->g + 1 + h(copy_up);
+                    newBond (openset, fRating, copy_up, idRating);
+            }
         }
         if (luka_y != 2)
         {
             newBoard(openset->board, copy_down, luka_y, luka_x, luka_y+1, luka_x);
-            fRating = openset->g + 1 + h(copy_down);
-            newBond (openset, fRating, copy_down);
+            idRating = id(copy_down);
+            if (!(alreadyInside(openset, idRating) || alreadyInside(closedset, idRating)))
+            {
+                fRating = openset->g + 1 + h(copy_down);
+                newBond (openset, fRating, copy_down, idRating);
+            }
         }
         if (luka_x != 0)
         {
             newBoard(openset->board, copy_left, luka_y, luka_x, luka_y, luka_x-1);
-            fRating = openset->g + 1 + h(copy_left);
-            newBond (openset, fRating, copy_left);
+            idRating = id(copy_left);
+            if (!(alreadyInside(openset, idRating) || alreadyInside(closedset, idRating)))
+            {
+                fRating = openset->g + 1 + h(copy_left);
+                newBond (openset, fRating, copy_left, idRating);
+            }
         }
         if (luka_x != 2)
         {
             newBoard(openset->board, copy_right, luka_y, luka_x, luka_y, luka_x+1);
-            fRating = openset->g + 1 + h(copy_right);
-            newBond (openset, fRating, copy_right);
+            idRating = id(copy_right);
+            if (!(alreadyInside(openset, idRating) || alreadyInside(closedset, idRating)))
+            {
+                fRating = openset->g + 1 + h(copy_right);
+                newBond (openset, fRating, copy_right, idRating);
+            }
         }
 
         transferToClosedset(closedset, openset);
@@ -95,6 +112,10 @@ int main()
         openset = openset->next;
         delete temporary;
     }
+
+    //bond *start = reconstructPath(openset);
+    extermination(openset);
+    extermination(closedset);
 
     return 0;
 }
@@ -147,7 +168,7 @@ void newBoard (int board[][3], int copy[][3], int luka_y, int luka_x, int obok_y
     copy[obok_y][obok_x] = bufor;
 }
 
-void newBond (bond *openset, int fRating, int board[][3])
+void newBond (bond *openset, int fRating, int board[][3], int idRating)
 {
     bond *fresh = new bond;
     fresh->g = openset->g+1;
@@ -156,7 +177,7 @@ void newBond (bond *openset, int fRating, int board[][3])
     for (int i=0; i<3; i++)
         for (int j=0; j<3; j++)
             fresh->board[i][j] = board[i][j];
-    fresh->id = id(fresh->board);
+    fresh->id = idRating;
     fresh->parent = openset;
 
     bond *topic = openset;
@@ -181,4 +202,43 @@ void transferToClosedset (bond *&closedset, bond *openset)
     *anew = *openset;
     anew->next = closedset;
     closedset = anew;
+}
+
+bool alreadyInside (bond *head, int id)
+{
+    while (head != NULL)
+    {
+        if (head->id == id)
+            return true;
+        head = head->next;
+    }
+    return false;
+}
+
+bond *reconstructPath (bond *openset)
+{
+    bond *head = new bond;
+    *head = *openset;
+    head->next = NULL;
+
+    while (head->parent != NULL)
+    {
+        bond *current = new bond;
+        *current = *(head->parent);
+        current ->next = head;
+        head = current;
+    }
+    return head;
+}
+
+void extermination (bond *&head)
+{
+    bond *topic = head;
+    while (topic != NULL)
+    {
+        bond *temporary = topic;
+        topic = topic->next;
+        delete temporary;
+    }
+    head = NULL;
 }
