ca41fdaa48b6214619ce308d849bc6abba58021a
Piotr Wachowicz <pwachowicz98@gmail.com>
2018-05-30 23:38:18 +0200

    first try

--- engine.cpp
+++ engine.cpp
@@ -5,6 +5,10 @@
 #include <ctime>
 #include <fstream>
 
+/// \brief odczytuje poczatkowe polozenie z pliku tekstowego
+/// \param nazwa nazwa pliku wraz z rozszerzeniem .txt
+/// \param matrix[][3] tablica do ktorej ma zostac przypisane wejsciowe ulozenie
+/// \return powodzenie lub niepowodzenie pobrania danych z pliku
 bool readFromFile(string nazwa, int matrix[][3])
 {
     ifstream input (nazwa);
@@ -19,7 +23,8 @@ bool readFromFile(string nazwa, int matrix[][3])
     return false;
 }
 
-//used to draw without repeats
+/// \brief Sprawdza czy liczba pojawia sie w tablicy do indexu i
+/// Wykorzystywana w funkcji draw aby sprawdzic czy element zostal juz wylosowany
 bool inside (int drawn, int table[], int n)
 {
     for (int i=0; i<n; i++)
@@ -30,7 +35,9 @@ bool inside (int drawn, int table[], int n)
     return false;
 }
 
-void draw(int table[], bool firstDraw)
+/// \brief losuje tablice z polozeniem poczatkowym
+/// \param table[] tablica do ktorej zostana wylosowane elementy
+void draw(int table[])
 {
     int drawn;
     for (int i=0; i<9; i++)
@@ -42,8 +49,14 @@ void draw(int table[], bool firstDraw)
     }
 }
 
+/**
+ * @brief solutionIsPosible sprawdza czy dane ulozenie ma rozwiazanie
+ * kazde ulozenie ma charakterystyczna liczbe permutacji. Ruchy zmieniaja ja o przysta ilosc. Do rozwiazania da sie doprowadzic tylko poloze ulozen poczatkowych
+ * \return ulozenie albo ma rozwiazanie albo go nie ma
+ */
 bool solutionIsPosible(int matrix[][3])
 {
+    ///liczba permutacaji to suma dla kazdej plytki ilosci mniejszych plytek wystepujacych dalej + wiersz w ktorym znajduje sie pusta
     int permutationInversions = 0;
     for (int i=0; i<3; i++)
         for (int j=0; j<3; j++)
@@ -65,6 +78,7 @@ bool solutionIsPosible(int matrix[][3])
     return false;
 }
 
+/// \brief sprawdzaczy zadne cyfry sie nie powtarzaja ani nie wystepuja niedozwolone znaki
 bool inRules(int matrix[][3])
 {
     bool seen[9];
@@ -78,7 +92,7 @@ bool inRules(int matrix[][3])
     return true;
 }
 
-//kopiuje element do listy elementow odwiedzonych. Towrzy tam NOWY element ktoremu przepisuje wartosci
+/// \brief kopiuje element do listy elementow zamknietych. Towrzy tam NOWY element ktoremu przepisuje wartosci
 void transferToClosedset (Node *&closedset, Node *openset)
 {
     Node *anew = new Node(openset->board);
@@ -87,7 +101,8 @@ void transferToClosedset (Node *&closedset, Node *openset)
     closedset = anew;
 }
 
-//sprawdza czy element juz jest w danej liscie
+/// \brief sprawdza czy element juz jest w danej liscie
+/// przed dodaniem do listy sprawdzam to aby nie storzyc dubla
 bool alreadyInside (Node *head, int id)
 {
     while (head != nullptr)
@@ -99,7 +114,7 @@ bool alreadyInside (Node *head, int id)
     return false;
 }
 
-//wykorzystywane do tworzenia nowego wezla, gdy szukam miejsca w ktorym go umiescic sortujac po fullDistance
+/// \brief wykorzystywane do tworzenia nowego wezla, gdy szukam miejsca w ktorym go umiescic sortujac po fullDistance
 bool insertHere (Node *fresh, Node *looking)
 {
     if (looking->next == nullptr)
@@ -109,7 +124,12 @@ bool insertHere (Node *fresh, Node *looking)
     return true;
 }
 
-//tworzy nowy wezel do liscie dostepnych
+/**
+ * @brief insertNode to insert node
+ * @param openset to openset
+ * @param parent to parent
+ * @param environment to environment
+ */
 void insertNode (Node *openset, Node *parent, Board environment)
 {
     Node *fresh = new Node(environment, parent);
